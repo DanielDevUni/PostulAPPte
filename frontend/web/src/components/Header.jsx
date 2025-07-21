e2bfdd34f86ea4938de2cc459ada13f362dd37d9
DanielDevUni <daniel.cardenas.ortiz@correounivalle.edu.co>
2025-07-21 18:30:09 -0500

    Resolviendo conflictos de un merge no deseado

--- frontend/web/src/components/Header.jsx
+++ frontend/web/src/components/Header.jsx
@@ -2,7 +2,6 @@ import Icon from './Icons';
 
 function NavbarAuth() {
   return (
-<<<<<<< HEAD
       <nav class="navbar navbar-expand-lg navbar-postulappte shadow">
         <div class="container">
             <a className="navbar-brand d-flex align-items-center" href="#">
@@ -26,30 +25,6 @@ function NavbarAuth() {
                   <a class="nav-link" href="/offers">Ofertas</a>
                 </li>
             </ul>
-=======
-    <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-            <a className="navbar-brand" href="#">PostulAPPte</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Ofertas</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Postulaciones</a>
-                </li>
-            </ul>
-            <span className="navbar-text">
-                Space to User
-            </span>
->>>>>>> 6a68097e31bbec786eda051083ca5d2112e8d605
             </div>
         </div>
       </nav>
